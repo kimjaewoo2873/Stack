b9c45682347469ff3155dc080c3660c54b59db00
jaewoo <jaewoo1427@naver.com>
2025-01-27 15:57:21 +0900

    동적배열스택

---
+++ 동적배열스택.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int element;
+typedef struct {
+	element* data; // 동적할당으로 크기를 늘려갈거니 포인터형으로 선언
+	int capacity;
+	int top;
+} StackType;
+
+void init_stack(StackType* s) {
+	s->top = -1;
+	s->capacity = 1; // 현재 크기
+	s->data = (element*)malloc(s->capacity * sizeof(element));
+}
+
+int is_empty(StackType* s) {
+	return s->top == -1;
+}
+
+int is_full(StackType* s) {
+	return s->top == (s->capacity - 1);
+}
+
+void push(StackType* s, element item) {
+	if (is_full(s)) {
+		s->capacity *= 2;
+		s->data = (element*)realloc(s->data, s->capacity * sizeof(element));
+	}
+	s->data[++(s->top)] = item;
+}
+
+element pop(StackType* s) {
+	if (is_empty(s)) {
+		fprintf(stderr, "스택 공백 에러\n");
+		exit(1);
+	}
+	else
+		return s->data[(s->top)--];
+}
+
+int main(void) {
+	StackType s;
+	init_stack(&s);
+	push(&s, 1);
+	push(&s, 2);
+	push(&s, 3);
+	printf("%d\n", pop(&s));
+	printf("%d\n", pop(&s));
+	printf("%d\n", pop(&s));
+	free(s.data); // 동적할당 반납
+}
